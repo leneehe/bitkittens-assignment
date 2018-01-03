1f7d9f072eb6ae21ead96887c6def5f49bcd4685
Lena <fatmeow@gmail.com>
2018-01-03 15:08:23 -0500

    Complete adding cats to the DOM

--- cats.js
+++ cats.js
@@ -1,8 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
   var catBoxes = document.querySelectorAll('.cat-box'),
-      summonBtn = document.querySelector('button.summon-cats');
+      summonBtn = document.querySelector('button.summon-cats'),
+      boxIndex = 0;
 
   summonBtn.addEventListener('click', function() {
-    console.log(this)
+    $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET',
+      dataType: 'json'
+    }).done(function(responseData) {
+      responseData['cats'].forEach(function(cat) {
+        if (boxIndex > 2) {
+          boxIndex = 0;
+        }
+        var img = document.createElement('img');
+        img.src = cat.photo;
+        img.alt = `Photo of ${cat.name}`;
+        catBoxes[boxIndex].innerHTML = "";
+        catBoxes[boxIndex].append(img);
+        boxIndex++;
+      })
+
+    })
   })
 })
